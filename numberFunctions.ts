fc5ebe777e9d835a62b9f4e7875a878206de2c63
Milena Georgieva <133628364+MilenaGeorgieva95@users.noreply.github.com>
2025-05-22 18:40:27 +0300

    add more functions

--- numberFunctions.ts
+++ numberFunctions.ts
@@ -8,7 +8,7 @@ function largestNumber(
 }
 
 function circleArea(input: number | string): string {
-  if (typeof input==="number") {
+  if (typeof input === "number") {
     const area: number = Math.pow(input, 2) * Math.PI;
     return area.toFixed(2);
   } else {
@@ -16,7 +16,11 @@ function circleArea(input: number | string): string {
   }
 }
 
-function mathOperations(numOne:number, numTwo:number, str:"+" | "-" | "*" | "/" | "%" | "**") : number {
+function mathOperations(
+  numOne: number,
+  numTwo: number,
+  str: "+" | "-" | "*" | "/" | "%" | "**"
+): number {
   switch (str) {
     case "+":
       return numOne + numTwo;
@@ -30,6 +34,66 @@ function mathOperations(numOne:number, numTwo:number, str:"+" | "-" | "*" | "/" 
       return numOne % numTwo;
     case "**":
       return numOne ** numTwo;
-    default: throw new Error("Invalid operator");
+    default:
+      throw new Error("Invalid operator");
   }
+}
+
+function sumOfNumbersNtoM(n: string, m: string): number {
+  let numN: number = Number(n);
+  let numM: number = Number(m);
+  let sum: number = 0;
+  for (let i = numN; i <= numM; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+function dayOfWeek(dayStr: string): number {
+  switch (dayStr) {
+    case "Monday":
+      return 1;
+    case "Tuesday":
+      return 2;
+    case "Wednesday":
+      return 3;
+    case "Thursday":
+      return 4;
+    case "Friday":
+      return 5;
+    case "Saturday":
+      return 6;
+    case "Sunday":
+      return 7;
+    default:
+      throw new Error("Invalid day!");
+  }
+}
+
+function daysInAMonth(monthNum: number, yearNum: number): number {
+  let endOfTheMonthDate: Date = new Date(yearNum, monthNum, 0);
+  let daysCount: number = endOfTheMonthDate.getDate();
+  return daysCount;
+}
+
+function squareOfStars(sizeNum: number = 5): void {
+  for (let i: number = 1; i <= sizeNum; i++) {
+    console.log("* ".repeat(sizeNum));
+  }
+}
+
+function agregateElements(arr: string[]): void {
+  let sum: number = 0;
+  let inverseSum: number = 0;
+  let concatenation: string = "";
+
+  for (let num of arr) {
+    let curNum: number = Number(num);
+    sum += curNum;
+    inverseSum += 1 / curNum;
+    concatenation += num;
+  }
+  console.log(sum);
+  console.log(inverseSum);
+  console.log(concatenation);
 }
